dbcca9b81766b87471b266f8552f2eb88bc633aa
xihuaxiao(肖西华) <xihuaxiao@tencent.com>
2023-05-30 19:31:20 +0800

    优化组合处理

--- zaplog.go
+++ zaplog.go
@@ -2,6 +2,7 @@ package log
 
 import (
 	"fmt"
+	"go.uber.org/zap"
 )
 
 // init 初始化
@@ -10,4 +11,23 @@ func init() {
 	if err := Init(c); err != nil {
 		fmt.Println(err)
 	}
+}
+
+/*
+封装相关方法后续通过包名直接调用
+*/
+
+// Infof info func
+func Infof(msg string, fields ...zap.Field) {
+	Log.Info(msg, fields...)
+}
+
+// Warnf info func
+func Warnf(msg string, fields ...zap.Field) {
+	Log.Warn(msg, fields...)
+}
+
+// Errorf info func
+func Errorf(msg string, fields ...zap.Field) {
+	Log.Error(msg, fields...)
 }
